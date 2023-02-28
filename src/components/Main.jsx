1863ba0d0faebf6a961e5a6d2f33e160a04459e6
frankmuthomi30 <frankmuthomi30@gmail.com>
2023-02-28 10:07:20 +0300

    deletbtn

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,41 @@
-import react from 'react';
-import { ReactDOM } from 'react-dom';
+import React, { useState } from 'react';
 import './Main.css';
-function Main (){
-    return(
-        <div className="Main">
-            <div className="container">
-                 <div className="box"> 
-                <h2>Welcome Back!</h2>
-                <button>Add</button>
-                 </div>
-             <ul>
-                <li><input type="checkbox" id="" /> feed the cows <button>delete</button> </li>
-                <li><input type="checkbox" id="" />go buy groceries</li>
-                <li><input type="checkbox" id="" />attend choir practice</li>
-             </ul>
-            
-            </div>
+
+function Main() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "feed the cows" },
+    { id: 2, text: "go buy groceries" },
+    { id: 3, text: "attend choir practice" }
+  ]);
+
+  function handleDelete(taskId) {
+    setTasks(tasks.filter(function(task) {
+      return task.id !== taskId;
+    }));
+  }
+
+  return (
+    <div className="Main">
+      <div className="container">
+        <div className="box"> 
+          <h2>Welcome Back!</h2>
+          <button>Add</button>
         </div>
-    )
+        <ul>
+          {tasks.map(function(task) {
+            return (
+              <li key={task.id}>
+                <input type="checkbox" id={task.id} />
+                {task.text}
+                <button onClick={function() { handleDelete(task.id) }}>delete</button>
+                <button>edit</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
-export default Main
+
+export default Main;
